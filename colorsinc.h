64c33d574d684e9c89b5705c0ed4b8ff5bf125d6
Wagnitz. <wagnitz@live.com>
2014-10-22 19:06:47 -0200

    Colors in C.

---
+++ colorsinc.h
@@ -0,0 +1,36 @@
+/*
+ *	Titulo:		Colors in C.
+ *	File:		colorsinc.h
+ *	Type:		Header.
+ *	Author:		W4G Development.
+ *	GitHub:		www.github.com/w4g
+ *	Date:		Oct 22 2014.
+ */
+
+// Colors.
+#define Black		30
+#define Red			31
+#define Green		32
+#define Yellow		33
+#define Blue		34
+#define Magenta		35
+#define Cyan		36
+#define White		37
+
+// Background.
+#define bg_black	40
+#define bg_red		41
+#define bg_green	42
+#define bg_yellow	43
+#define bg_blue		44
+#define bg_magenta	45
+#define bg_cyan		46
+#define bg_white	47
+
+// Attributes.
+#define all_att_off	0
+#define bold		1
+#define underscore	4
+#define blink		5
+#define reverse_v	7
+#define concelead	8
